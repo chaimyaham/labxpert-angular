e3b9a4905e7228ea7535c89a682b84e0fe193fde
aitnacer-nabil <palancia.gor@gmail.com>
2024-01-28 12:41:44 +0100

    echantillon add and list

---
+++ src/app/components/echantillon/list-echantillon/list-echantillon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-echantillon',
+  templateUrl: './list-echantillon.component.html',
+  styleUrls: ['./list-echantillon.component.css']
+})
+export class ListEchantillonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
