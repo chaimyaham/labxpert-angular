6915beec983dc60ea7c689e607e5c7a6d8b4b80e
aitnacer-nabil <118471449+aitnacer-nabil@users.noreply.github.com>
2024-01-30 11:14:45 +0100

    Revert "add function for add echantillon" (#6)

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Patient} from "../models/patient";
+import {Patient} from "../model/patient";
 import {Observable} from "rxjs";
 
 @Injectable({
